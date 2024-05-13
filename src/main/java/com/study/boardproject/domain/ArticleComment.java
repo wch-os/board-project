86785b8d9e73c5e083bd5547bb6bf50613494bb2
wch-os <wch1224.s@gmail.com>
2024-05-13 22:31:17 +0900

    #10 - Domain을 Entity로 변환
    1) 1 Domain 클래스 == 1 Table 매칭되도록 설계했다.
    2) 생성자를 만들 때, 명시성과 확장성을 위해 Factory Method 패턴을 사용했다.
    3) `equals()`, `hashCode()`를 id 비교와 영속성에 맞게끔 재정의했다.

--- src/main/java/com/study/boardproject/domain/ArticleComment.java
+++ src/main/java/com/study/boardproject/domain/ArticleComment.java
@@ -1,14 +1,61 @@
 package com.study.boardproject.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+@Entity
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@EntityListeners(AuditingEntityListener.class)
 public class ArticleComment {
-    private Long id;
-    private Article article; // 게시글 (ID)
-    private String content; // 내용
 
-    private LocalDateTime createdAt; // 생성일시
-    private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+
+    // optional = false : 관계를 맺고 있는 엔티티 객체가 반드시 존재해야 한다.
+    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+
+    @Setter @Column(nullable = false, length = 500) private String content; // 내용
+
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    // 팩터리 메서드
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+    return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
