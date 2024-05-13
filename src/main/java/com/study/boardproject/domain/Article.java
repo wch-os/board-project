9cc5410e46165c10d157600d5ccd26c70ead933b
wch-os <wch1224.s@gmail.com>
2024-05-14 01:09:50 +0900

    #12 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시의 필드는 반복적으로 엔티티 클래스에 들어가는 요소이며, 도메인과 직접 연관이 없는 요소이므로 추출하였다.
    
    `@MappedSuperclass` 를 이용하여 상속 방식으로 추출함
    
    그 외 `@DateTimeFormat`포맷팅 지정 및 jpa 옵션 개선(updatable=false)

--- src/main/java/com/study/boardproject/domain/Article.java
+++ src/main/java/com/study/boardproject/domain/Article.java
@@ -25,8 +25,7 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
-@EntityListeners(AuditingEntityListener.class) // Auditing 사용
-public class Article {
+public class Article extends AuditingFields{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -49,18 +48,6 @@ public class Article {
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt; // 생성일시
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy; // 생성자
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy; // 수정자
 
     protected Article() {
     }
--- src/main/java/com/study/boardproject/domain/ArticleComment.java
+++ src/main/java/com/study/boardproject/domain/ArticleComment.java
@@ -20,8 +20,7 @@ import java.util.Objects;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
-@EntityListeners(AuditingEntityListener.class)
-public class ArticleComment {
+public class ArticleComment extends AuditingFields{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
 
@@ -30,11 +29,6 @@ public class ArticleComment {
 
     @Setter @Column(nullable = false, length = 500) private String content; // 내용
 
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
 
 
     private ArticleComment(Article article, String content) {
