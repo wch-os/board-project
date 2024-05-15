4ff3b4b9f9f63ef4fda5463bdfd358e4fff55608
wch-os <wch1224.s@gmail.com>
2024-05-16 02:46:14 +0900

    #16 - 로그인 컨트롤러 테스트 + 컨트롤러 테스트에 Security 설정 반영
    Spring Security가 자동 생성해준 `/login` 컨트롤러를 테스트, 그리고 모든 컨트롤러가 Security 설정을 반영할 수 있도록 `@Import`를 추가한다.

--- src/test/java/com/study/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/study/boardproject/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.study.boardproject.controller;
 
+import com.study.boardproject.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -13,6 +15,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
     @Autowired
